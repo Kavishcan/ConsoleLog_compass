4335fea84dfbd5c9717a4fad00ccd526827ca855
Dasantha Edirisinghe <114149683+Tharindu-Dasantha@users.noreply.github.com>
2024-09-04 02:24:38 +0530

    added the items to the hero section

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,18 +1,72 @@
-import { View, Text, Image } from "react-native";
 import React from "react";
+import { View, Text, Image } from "react-native";
+import { styled, withExpoSnack } from "nativewind";
 
-export default function Hero() {
+// icons
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
+
+const Hero = () => {
   return (
-    <View>
-      <View className="">
-        <View className="bg-blue-900">
+    <StyledView className="mt-10">
+      <StyledView className="flex flex-row justify-between p-3 px-5 items-center">
+        <StyledView className="flex flex-row gap-3 items-center">
           <Image source={require("../../assets/images/home/avatar.png")} />
-          <Text className="text-white">Hi, Bella</Text>
-        </View>
-        <View>
-            <Image source={require("../../assets/images/home/notification.png")} />
-        </View>
-      </View>
-    </View>
+          <Text className="text-white text-xl">Hi, Bella</Text>
+        </StyledView>
+        <StyledView>
+          {/* replace with icon where without notification no red dot with notification red dot */}
+          {/* <Image source={require("../../assets/images/home/notification.png")} /> */}
+          <Ionicons name="notifications-outline" size={30} color="white" />
+        </StyledView>
+      </StyledView>
+
+      <StyledView className="w-screen flex bg-blue-900 justify-center items-center">
+        {/* first row */}
+        <StyledView className="flex flex-row gap-4">
+          <StyledView className="flex flex-row justify-center items-center bg-[#EB7400] w-[70%] p-2 py-6 rounded-2xl gap-1">
+            <Ionicons name="paper-plane-outline" size={50} color="white" />
+            <StyledText className="text-white text-xl font-bold">
+              Quick Guide
+            </StyledText>
+          </StyledView>
+          <StyledView className="bg-white rounded-2xl w-[40%] flex justify-center items-center">
+            <MaterialIcons name="emergency" size={50} color="black" />
+          </StyledView>
+        </StyledView>
+
+        {/* Second row */}
+        <StyledView className="flex flex-row gap-4">
+          <StyledView className="bg-white rounded-2xl w-[40%] flex justify-center items-center">
+            <MaterialIcons name="emergency" size={50} color="black" />
+          </StyledView>
+          <StyledView className="flex flex-row justify-center items-center bg-[#EB7400] w-[70%] p-2 py-6 rounded-2xl gap-1">
+            <Ionicons name="paper-plane-outline" size={50} color="white" />
+            <StyledText className="text-white text-xl font-bold">
+              Quick Guide
+            </StyledText>
+          </StyledView>
+        </StyledView>
+
+        {/* Third row */}
+        <StyledView className="flex flex-row gap-4">
+          <StyledView className="flex flex-row justify-center items-center bg-[#EB7400] w-[70%] p-2 py-6 rounded-2xl gap-1">
+            <Ionicons name="paper-plane-outline" size={50} color="white" />
+            <StyledText className="text-white text-xl font-bold">
+              Quick Guide
+            </StyledText>
+          </StyledView>
+          <StyledView className="bg-white rounded-2xl w-[40%] flex justify-center items-center">
+            <MaterialIcons name="emergency" size={50} color="black" />
+          </StyledView>
+        </StyledView>
+      </StyledView>
+    </StyledView>
   );
-}
+};
+
+export default withExpoSnack(Hero);
